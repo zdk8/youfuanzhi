c242e8515c3a93c648ed8cce72b8b310efccc31a
tds <tds@sina.com>
2016-12-16 10:51:16 +0800

    组织结构的内容调整

--- src/main/webapp/apps/lishui2qi/views/WorkFlow.js
+++ src/main/webapp/apps/lishui2qi/views/WorkFlow.js
@@ -3,7 +3,7 @@ define([],function () {
     
     return {
         render:function (local) {
-            local.find('img').attr('src', encodeURI('images/丽水应急机构.png'));
+            local.find('img').attr('src', encodeURI('images/丽水应急机构2.png'));
         }
     }
 });
